d54cc0250a98028c2a3fb5c2146586154a499695
Nick Pruitt <nick.s.pruitt@gmail.com>
2019-11-15 16:53:21 -0600

    Edit 1

--- app.js
+++ app.js
@@ -14,16 +14,30 @@ $('#title').on('click', function(event){
 
 // Hide the “Select Theme” `option` element in the “Design” menu.
 $('#design option').eq(0).hide();
+
 // Update the “Color” field to read “Please select a T-shirt theme”.
 $('#color').prepend('<option selected>Please select a T-Shirt theme</option>');
 $('#color option')[0].selectedIndex = 0; 
+
 // Hide the colors in the “Color” drop down menu
 $('#color option:gt()').hide();
+
 // when one of the two themes is selected, only the appropriate colors should show in the “Color” drop down menu
     // use a `change` event listener on the “Design” menu `select` element to listen for changes.
-    // inside the event listener, you’ll use a conditional to determine what to hide, show and update. 
+    // inside the event listener, you’ll use a conditional to determine what to hide, show and update.
 $('#design').change(function(e) {
     if (e.target === 'selected'){
     $('#color option:lt(5)').show();
     };  
+});
+
+//activity section
+
+let domElm = $('<div></div>');
+domElm.appendTo('.activities');
+
+let activityCost = '0';
+$('.activities').change(function(){
+    let act = $('.activites:checkbox').html();
+    console.log(act);
 });
